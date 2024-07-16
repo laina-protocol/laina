fd09dc7daa361c27b520ffceaca40aaa29c2def2
Teolhyn <84379059+Teolhyn@users.noreply.github.com>
2024-07-16 14:18:16 +0300

    Delete src/contracts directory
    Deleted as it is generated when the product is initialized and it has network keys visible

--- src/contracts/loan_pool.ts
+++
@@ -1,9 +0,0 @@
-import * as Client from 'loan_pool';
-import { rpcUrl } from './util';
-
-export default new Client.Client({
-  ...Client.networks.testnet,
-  rpcUrl,
-  allowHttp: true,
-  publicKey: 'GBRPYHIL2CI3FNQ4BXLFMNDLFJUNPU2HY3ZMFSHONUCEOASW7QC7OX2H',
-});
