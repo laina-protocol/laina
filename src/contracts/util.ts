fa756f30568e93d2ea13eaff44d50b4b532bd91b
Konsta Purtsi <konsta@arado.fi>
2024-08-18 14:34:08 +0300

    feat: add rust build & test action, switch to prettier formatting

--- src/contracts/util.ts
+++ src/contracts/util.ts
@@ -1,5 +1,3 @@
-export const rpcUrl =
-	import.meta.env.PUBLIC_SOROBAN_RPC_URL ?? "http://localhost:8000/rpc";
+export const rpcUrl = import.meta.env.PUBLIC_SOROBAN_RPC_URL ?? 'http://localhost:8000/rpc';
 export const networkPassphrase =
-	import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE ??
-	"Standalone Network ; February 2017";
+  import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE ?? 'Standalone Network ; February 2017';
