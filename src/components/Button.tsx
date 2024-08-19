906d326f98b02bd7b80c1e9172616058fc7e24de
Konsta Purtsi <konsta@arado.fi>
2024-08-19 15:51:35 +0300

    feat: Style lendable assets.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,20 @@
 import type { PropsWithChildren } from 'react';
 
+export interface ButtonProps {
+  onClick: () => void;
+  className?: string;
+}
+
+export const Button = ({ onClick, className = '', children }: PropsWithChildren<ButtonProps>) => (
+  <button
+    type="button"
+    onClick={onClick}
+    className={`bg-black text-white rounded-full px-8 py-2 hover:bg-grey-dark transition ${className}`}
+  >
+    {children}
+  </button>
+);
+
 export const SelectButtonWrapper = ({ children }: PropsWithChildren) => (
   <div className="flex bg-grey p-1 gap-2 rounded-full">{children}</div>
 );
@@ -10,7 +25,10 @@ export interface SelectLinkButtonProps {
 }
 
 export const SelectLinkButton = ({ href, selected, children }: PropsWithChildren<SelectLinkButtonProps>) => (
-  <a className={`font-bold px-6 py-1.5 rounded-full ${selected ? 'bg-white text-black' : 'text-white'}`} href={href}>
+  <a
+    className={`font-bold px-6 py-1.5 rounded-full hover:text-black transition ${selected ? 'bg-white text-black' : 'text-white'}`}
+    href={href}
+  >
     {children}
   </a>
 );
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,10 +8,27 @@ export default {
     colors: {
       white: '#fff',
       grey: {
+        dark: '#555',
         DEFAULT: '#999',
         light: '#efefef',
       },
       black: '#000',
+    },
+    fontSize: {
+      sm: ['16px', '20px'],
+      base: ['18px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['26px', '32px'],
+      '2xl': ['32px', '40px'],
+      '3xl': ['48px', '56px'],
+    },
+    borderRadius: {
+      none: '0',
+      DEFAULT: '16px',
+      full: '9999px',
+    },
+    dropShadow: {
+      DEFAULT: '0 4px 4px rgba(0, 0, 0, 0.25)',
     },
     extend: {
       spacing: {
