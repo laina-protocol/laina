4519dbe9af73be280c731cbb427b5aeb64d2ee24
Konsta Purtsi <konsta@arado.fi>
2024-09-11 14:55:33 +0300

    feat: Make index page bg white, add index nav.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Outlet, RouterProvider, createBrowserRouter, useLocation } from 'react-router-dom';
 
 import Nav from '@components/Nav';
 import BorrowPage from '@pages/_borrow/BorrowPage';
@@ -8,14 +8,20 @@ import LendPage from '@pages/_lend/LendPage';
 import LiquidatePage from '@pages/_liquidate/LiquidatePage';
 import { WalletProvider } from './stellar-wallet';
 
-const PageWrapper = () => (
-  <>
-    <Nav />
-    <main className="max-w-screen w-256">
-      <Outlet />
-    </main>
-  </>
-);
+const PageWrapper = () => {
+  const { pathname } = useLocation();
+
+  const isIndex = pathname === '/';
+
+  return (
+    <body className={`font-sans min-h-screen ${isIndex ? 'bg-white' : 'bg-grey-light'}`}>
+      < Nav />
+      <main className="max-w-screen w-256">
+        <Outlet />
+      </main>
+    </body >
+  );
+};
 
 const router = createBrowserRouter([
   {
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,30 +2,22 @@ import { Link, useLocation } from 'react-router-dom';
 import { useWallet } from 'src/stellar-wallet';
 import logo from '/public/laina_v3_shrinked.png';
 import { Button, SelectButtonWrapper, SelectLinkButton } from './Button';
+import type { PropsWithChildren } from 'react';
 
 export default function Nav() {
   const { pathname } = useLocation();
   const { wallet, openConnectWalletModal } = useWallet();
 
+  const isIndex = pathname === '/';
+
   return (
     <nav className="relative max-w-screen-lg mx-auto mb-12 flex justify-between items-center pt-12 pb-6">
       <div>
         <Link to="/">
-          <img src={logo.src} alt="logo" width={200} />
-        </Link>
+          <img src={logo.src} alt="logo" width={200} /></Link>
       </div>
 
-      <SelectButtonWrapper>
-        <SelectLinkButton to="/lend" selected={pathname === '/lend'}>
-          Lend
-        </SelectLinkButton>
-        <SelectLinkButton to="/borrow" selected={pathname === '/borrow'}>
-          Borrow
-        </SelectLinkButton>
-        <SelectLinkButton to="/liquidate" selected={pathname === '/liquidate'}>
-          Liquidate
-        </SelectLinkButton>
-      </SelectButtonWrapper>
+      {isIndex ? <LinkCluster /> : <SelectButtonCluster pathname={pathname} />}
 
       {wallet ? (
         <div>
@@ -38,3 +30,31 @@ export default function Nav() {
     </nav>
   );
 }
+
+const LinkCluster = () => (
+  <div className='flex flex-row'>
+    <LinkItem to="/lend">Lend</LinkItem>
+    <LinkItem to="/borrow">Borrow</LinkItem>
+    <LinkItem to="/liquidate">Liquidate</LinkItem>
+  </div>
+);
+
+const LinkItem = ({ to, children }: PropsWithChildren<{ to: string }>) => (
+  <Link to={to} className="font-semibold p-4 hover:underline">
+    {children}
+  </Link>
+)
+
+const SelectButtonCluster = ({ pathname }: { pathname: string }) => (
+  <SelectButtonWrapper>
+    <SelectLinkButton to="/lend" selected={pathname === '/lend'}>
+      Lend
+    </SelectLinkButton>
+    <SelectLinkButton to="/borrow" selected={pathname === '/borrow'}>
+      Borrow
+    </SelectLinkButton>
+    <SelectLinkButton to="/liquidate" selected={pathname === '/liquidate'}>
+      Liquidate
+    </SelectLinkButton>
+  </SelectButtonWrapper>
+);
