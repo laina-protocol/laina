ae4580ce77e52e429ee95473f77892bfdabab9dd
Konsta Purtsi <konsta@arado.fi>
2024-10-02 14:26:27 +0300

    refactor: Use 1 cent accuracy when formatting to dollars.

--- src/lib/formatting.ts
+++ src/lib/formatting.ts
@@ -1,8 +1,14 @@
 // 7 decimal numbers is the smallest unit of XLM, stroop.
 const SCALAR_7 = 10_000_000n;
+const CENTS_SCALAR = SCALAR_7 * 100_000n;
 
 const TEN_K = 10_000n * SCALAR_7;
 const ONE_M = 1_000_000n * SCALAR_7;
+
+// 10 thousand dollars = 1 million cents
+const TEN_K_CENTS = 10_000n * 100n;
+// 1 million dollars = 100 million cents
+const ONE_M_CENTS = 1_000_000n * 100n;
 
 export const formatAmount = (amount: bigint): string => {
   if (amount === 0n) return '0';
@@ -18,18 +24,21 @@ export const formatAmount = (amount: bigint): string => {
 
 export const toDollarsFormatted = (price: bigint, amount: bigint) => {
   if (amount === 0n) return '$0';
-  return formatDollarAmount(toDollars(price, amount));
+  return formatCentAmount(toCents(price, amount));
 };
 
-export const toDollars = (price: bigint, amount: bigint) => ((price / SCALAR_7) * amount) / SCALAR_7;
+export const toCents = (price: bigint, amount: bigint) => {
+  return (price * amount) / CENTS_SCALAR;
+};
 
-export const formatDollarAmount = (amount: bigint) => {
-  if (amount === 0n) return '$0';
-  if (amount > ONE_M) {
-    return `$${(Number(amount) / (1_000_000 * 10_000_000)).toFixed(2)}M`;
+export const formatCentAmount = (cents: bigint) => {
+  if (cents === 0n) return '$0';
+
+  if (cents > ONE_M_CENTS) {
+    return `$${(Number(cents) / 100_000_000).toFixed(2)} M`;
   }
-  if (amount > TEN_K) {
-    return `$${(Number(amount) / (1_000 * 10_000_000)).toFixed(1)}K`;
+  if (cents > TEN_K_CENTS) {
+    return `$${(Number(cents) / 100_000).toFixed(2)} K`;
   }
-  return `$${(Number(amount) / 10_000_000).toFixed(1)}`;
+  return `$${(Number(cents) / 100).toFixed(1)} `;
 };
