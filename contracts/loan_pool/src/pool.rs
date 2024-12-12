a0a363ed9067f304fa85e97dd2dc2439ca1cad7d
teolhyn <teemu.hynna@gmail.com>
2024-12-12 10:50:59 +0200

    refactor: remove same postfixes from error enum variants

--- contracts/loan_pool/src/pool.rs
+++ contracts/loan_pool/src/pool.rs
@@ -11,14 +11,14 @@ pub struct Currency {
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 #[repr(u32)]
 pub enum Error {
-    LoanManagerNotFound = 1,
-    CurrencyNotFound = 2,
-    LiquidationThresholdNotFound = 3,
-    TotalSharesNotFound = 4,
-    TotalBalanceNotFound = 5,
-    AvailableBalanceNotFound = 6,
-    AccrualNotFound = 7,
-    AccrualLastUpdatedNotFound = 8,
+    LoanManager = 1,
+    Currency = 2,
+    LiquidationThreshold = 3,
+    TotalShares = 4,
+    TotalBalance = 5,
+    AvailableBalance = 6,
+    Accrual = 7,
+    AccrualLastUpdated = 8,
 }
 
 pub fn write_loan_manager_addr(e: &Env, loan_manager_addr: Address) {
@@ -34,7 +34,7 @@ pub fn read_loan_manager_addr(e: &Env) -> Result<Address, Error> {
     if let Some(loan_manager_address) = e.storage().persistent().get(&key) {
         loan_manager_address
     } else {
-        Err(Error::LoanManagerNotFound)
+        Err(Error::LoanManager)
     }
 }
 
@@ -51,7 +51,7 @@ pub fn read_currency(e: &Env) -> Result<Currency, Error> {
     if let Some(currency) = e.storage().persistent().get(&key) {
         currency
     } else {
-        Err(Error::CurrencyNotFound)
+        Err(Error::Currency)
     }
 }
 
@@ -75,7 +75,7 @@ pub fn read_total_shares(e: &Env) -> Result<i128, Error> {
     if let Some(total_shares) = e.storage().persistent().get(&key) {
         total_shares
     } else {
-        Err(Error::TotalSharesNotFound)
+        Err(Error::TotalShares)
     }
 }
 
@@ -99,7 +99,7 @@ pub fn read_total_balance(e: &Env) -> Result<i128, Error> {
     if let Some(total_balance) = e.storage().persistent().get(&key) {
         total_balance
     } else {
-        Err(Error::TotalBalanceNotFound)
+        Err(Error::TotalBalance)
     }
 }
 
@@ -123,7 +123,7 @@ pub fn read_available_balance(e: &Env) -> Result<i128, Error> {
     if let Some(available_balance) = e.storage().persistent().get(&key) {
         available_balance
     } else {
-        Err(Error::AvailableBalanceNotFound)
+        Err(Error::AvailableBalance)
     }
 }
 
@@ -147,7 +147,7 @@ pub fn read_accrual(e: &Env) -> Result<i128, Error> {
     if let Some(accrual) = e.storage().persistent().get(&key) {
         accrual
     } else {
-        Err(Error::AccrualNotFound)
+        Err(Error::Accrual)
     }
 }
 
@@ -164,6 +164,6 @@ pub fn read_accrual_last_updated(e: &Env) -> Result<u64, Error> {
     if let Some(accrual_last_updated) = e.storage().persistent().get(&key) {
         accrual_last_updated
     } else {
-        Err(Error::AccrualLastUpdatedNotFound)
+        Err(Error::AccrualLastUpdated)
     }
 }
