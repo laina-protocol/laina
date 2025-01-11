b0d98f7d8509954e30a4d07eca0bbb5205b102bb
Konsta Purtsi <konsta@arado.fi>
2025-01-11 15:29:52 +0200

    feat: Add socials to footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,36 @@
+import type { ComponentType } from 'react';
+import { FaDiscord, FaGithub, FaXTwitter } from 'react-icons/fa6';
+
 export default function Footer() {
   return (
-    <footer className="bg-black px-4 h-40 flex flex-col items-center justify-center">
-      <p className="text-white w-[72rem] max-w-full text-lg">This website does not use cookies.</p>
-      <p className="text-white w-[72rem] max-w-full text-sm">&copy; Copyright 2024, Laina Protocol</p>
+    <footer className="bg-black px-4 flex flex-col items-center justify-center">
+      <div className="flex flex-row w-[74rem] max-w-full flex-wrap items-center mb-20">
+        <div className="md:w-1/2 flex flex-col pt-10 px-4">
+          <SocialLink href="https://discord.com/invite/gnUAFr3fUv" text="Laina Discord" Icon={FaDiscord} />
+          <SocialLink href="https://x.com/Lainadefi" text="@Lainadefi" Icon={FaXTwitter} />
+          <SocialLink href="https://github.com/laina-protocol" text="laina-protocol" Icon={FaGithub} />
+        </div>
+        <div className="md:w-1/2 pt-10 px-4 md:pl-14">
+          <p className="text-white max-w-full text-lg">This website does not use cookies.</p>
+          <p className="text-white max-w-full text-sm">&copy; Copyright 2025, Laina Protocol</p>
+        </div>
+      </div>
     </footer>
   );
 }
+
+interface IconProps {
+  className?: string;
+  size?: number;
+}
+
+const SocialLink = ({ href, text, Icon }: { href: string; text: string; Icon: ComponentType<IconProps> }) => {
+  return (
+    <span className="flex flex-row items-center my-1">
+      <Icon className="text-white hover:text-grey mr-2" size={32} />
+      <a className="text-lg text-white cursor-pointer hover:underline" href={href} target="_blank" rel="noreferrer">
+        {text}
+      </a>
+    </span>
+  );
+};
