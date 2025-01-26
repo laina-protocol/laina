b1e18a6041ba64f96ec6a4b6d788d7887b8ac6a3
Konsta Purtsi <konsta@arado.fi>
2025-01-26 09:05:58 +0200

    feat: Add awards to the landing page.

--- src/pages/_landing/LandingPage.tsx
+++ src/pages/_landing/LandingPage.tsx
@@ -1,6 +1,8 @@
 import { LinkButton, NativeLinkButton } from '@components/Button';
 import CardsImage from '@images/cards.jpg';
 import Konsta from '@images/konsta.jpeg';
+import ReflectorLogo from '@images/reflector.svg';
+import SCFLogo from '@images/scf.svg';
 import Teemu from '@images/teemu.jpg';
 import { FaDiscord, FaGithub } from 'react-icons/fa';
 
@@ -35,7 +37,11 @@ const LandingPage = () => (
         <img src={CardsImage.src} alt="" />
       </div>
     </section>
-    <section className="flex flex-row flex-wrap my-14">
+    <section className="flex flex-row flex-wrap my-10 md:mb-24">
+      <Award image={SCFLogo.src} title="Stellar Community Fund" subtitle="Kickstart Award" />
+      <Award image={ReflectorLogo.src} title="Reflector Dev Contest" subtitle="Best Oracle-based Smart Contract" />
+    </section>
+    <section className="flex flex-row flex-wrap my-10">
       <div className="md:w-1/2 py-14 md:pr-14 border-t-4">
         <p className="text-lg md:text-2xl leading-relaxed mb-8 tracking-tight">
           Laina is currently in the early development stage and is only operating on the Stellar Testnet.
@@ -75,6 +81,22 @@ const LandingPage = () => (
       </div>
     </section>
   </>
+);
+
+interface AwardProps {
+  image: string;
+  title: string;
+  subtitle: string;
+}
+
+const Award = ({ image, title, subtitle }: AwardProps) => (
+  <div className="flex items-center md:w-1/2 my-4 md:px-14">
+    <img src={image} alt="" className="h-14 w-14 md:h-16 md:w-16 mr-4" />
+    <div>
+      <p className="text-lg md:text-xl font-semibold tracking-tight">{title}</p>
+      <p className="md:text-lg tracking-tight">{subtitle}</p>
+    </div>
+  </div>
 );
 
 interface TeamMemberProps {
