561f248df2a8b6272178eb07a2ea010c2e34619a
Konsta Purtsi <konsta@arado.fi>
2025-02-05 18:06:38 +0200

    fix: Dialog cannot be a child of tr.

--- src/pages/_borrow/BorrowModal/BorrowModal.tsx
+++ src/pages/_borrow/BorrowModal/BorrowModal.tsx
@@ -8,13 +8,18 @@ import { TrustLineStep } from './TrustlineStep';
 export interface BorrowModalProps {
   modalId: string;
   onClose: () => void;
-  currency: CurrencyBinding;
+  currency: CurrencyBinding | null;
 }
 
 export const BorrowModal = ({ modalId, onClose, currency }: BorrowModalProps) => {
-  const { ticker } = currency;
   const { walletBalances, refetchBalances } = useWallet();
 
+  if (!currency) {
+    // Return an empty dialog if no currency set to make displaying the modal still work.
+    return <Dialog modalId={modalId} onClose={onClose} />;
+  }
+
+  const { ticker } = currency;
   const isTrustline = walletBalances?.[ticker].trustLine;
   // state to keep the user in the trustline step until they click ok.
   const [isSettingTrustline, setIsSettingTrustline] = useState(false);
